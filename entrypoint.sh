be2a9ce53c5346d1b444f44a177e28627bc82bab
Rafael Ribeiro <rafael.ntw@gmail.com>
2021-12-16 22:05:57 +0000

    feat: add docker support

---
+++ entrypoint.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+# parse environment variables
+if [[ -z "${VENDOR}" ]]; then
+  echo "No VENDOR environment variable supplied, assuming vendor RAK for the reset_lgw.sh script."
+  VENDOR="RAK"
+else
+  VENDOR="${VENDOR}"
+fi
+
+if [[ -z "${REGION}" ]]; then
+  echo "No REGION environment variable supplied, assuming region US915."
+  REGION="US915"
+else
+  REGION="${REGION}"
+fi
+
+if [[ -z "${CONCENTRATOR_MODEL}" ]]; then
+  echo "No CONCENTRATOR_MODEL environment variable supplied, assuming concentrator model sx1250."
+  CONCENTRATOR_MODEL="sx1250"
+else
+  CONCENTRATOR_MODEL="${CONCENTRATOR_MODEL}"
+fi
+
+if [[ -v "${CONCENTRATOR_INTERFACE}" ]]; then
+  echo "No CONCENTRATOR_INTERFACE environment variable supplied, assuming concentrator is connected via SPI."
+  CONCENTRATOR_INTERFACE=""
+elif [ "${CONCENTRATOR_INTERFACE}" == "spi" ] || [ "${CONCENTRATOR_INTERFACE}" == "SPI" ]; then
+  CONCENTRATOR_INTERFACE=""
+elif [ "${CONCENTRATOR_INTERFACE}" == "usb" ] || [ "${CONCENTRATOR_INTERFACE}" == "USB" ]; then
+  CONCENTRATOR_INTERFACE="USB"
+fi
+
+# copy configuration file based on passed environment variables
+if [ ! -f /opt/packet_forwarder/configs/global_conf.json ]; then
+  if [ "${CONCENTRATOR_INTERFACE}" == "USB" ]; then
+    cp /opt/packet_forwarder/default_configs/global_conf.json.$CONCENTRATOR_MODEL.$REGION.$CONCENTRATOR_INTERFACE /opt/packet_forwarder/configs/global_conf.json
+  else
+    cp /opt/packet_forwarder/default_configs/global_conf.json.$CONCENTRATOR_MODEL.$REGION /opt/packet_forwarder/configs/global_conf.json
+  fi
+fi
+
+# copy concentrator reset script based on passed environment variable
+if [ ! -f /opt/packet_forwarder/reset_lgw.sh ]; then
+  cp /opt/packet_forwarder/tools/reset_lgw.sh.$VENDOR /opt/packet_forwarder/reset_lgw.sh
+fi
+
+# run the packet forwarder
+/opt/packet_forwarder/lora_pkt_fwd -c /opt/packet_forwarder/configs/global_conf.json
