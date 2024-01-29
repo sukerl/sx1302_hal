71bd0bd061f6fcb97ccbd2d65be465c27d588539
Florian Dickbauer <sukerl@gmx.at>
2024-01-29 21:13:22 +0100

    asdf

--- packet_forwarder/src/lora_pkt_fwd.c
+++ packet_forwarder/src/lora_pkt_fwd.c
@@ -2067,11 +2067,21 @@ void thread_up(void)
 
     while (!exit_sig && !quit_sig)
     {
+        struct timeval beforeReceiveTime;
+        gettimeofday(&beforeReceiveTime, NULL);
+        int milliBeforeReceiveTime = beforeReceiveTime.tv_usec / 1000;
+        MSG("MYINFO: before lgw_receive: %d \n", milliBeforeReceiveTime);
 
         /* fetch packets */
         pthread_mutex_lock(&mx_concent);
         nb_pkt = lgw_receive(NB_PKT_MAX, rxpkt);
         pthread_mutex_unlock(&mx_concent);
+
+        struct timeval afterReceiveTime;
+        gettimeofday(&afterReceiveTime, NULL);
+        int milliAfterReceiveTime = afterReceiveTime.tv_usec / 1000;
+        MSG("MYINFO: after lgw_receive: %d \n", milliAfterReceiveTime);
+
         if (nb_pkt == LGW_HAL_ERROR)
         {
             MSG("ERROR: [up] failed packet fetch, exiting\n");
